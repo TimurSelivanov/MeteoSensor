e29bb8791aa341acb5cd79c8eb2a6cffb123c0f9
TimSelivan <timselivanson@gmail.com>
2023-04-05 14:26:16 +0300

    init commit

---
+++ src/main/java/com/selivanov/MeteoSensor/MeteoSensorApplication.java
@@ -0,0 +1,13 @@
+package com.selivanov.MeteoSensor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeteoSensorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeteoSensorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/selivanov/MeteoSensor/MeteoSensorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.selivanov.MeteoSensor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeteoSensorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
