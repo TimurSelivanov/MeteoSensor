340cde95c44e70d515771c1468b0e6f5460a4ec5
TimSelivan <timselivanson@gmail.com>
2023-04-05 15:50:32 +0300

    Add Measurement.java to model layer

---
+++ src/main/java/com/selivanov/MeteoSensor/model/Measurement.java
@@ -0,0 +1,76 @@
+package com.selivanov.MeteoSensor.model;
+
+import javax.persistence.*;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "measurement")
+public class Measurement {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "value")
+    @NotNull
+    @Min(-50)
+    @Max(50)
+    private Double value;
+
+    @Column(name = "is_raining")
+    @NotNull
+    private Boolean isRaining;
+
+    @Column(name = "measurement_date_time")
+    @NotNull
+    private LocalDateTime measurementDateTime;
+
+    @JoinColumn(name = "sensor", referencedColumnName = "name")
+    @ManyToOne
+    @NotNull
+    private Sensor sensor;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Double getValue() {
+        return value;
+    }
+
+    public void setValue(Double value) {
+        this.value = value;
+    }
+
+    public Boolean getRaining() {
+        return isRaining;
+    }
+
+    public void setRaining(Boolean raining) {
+        isRaining = raining;
+    }
+
+    public LocalDateTime getMeasurementDateTime() {
+        return measurementDateTime;
+    }
+
+    public void setMeasurementDateTime(LocalDateTime measurementDateTime) {
+        this.measurementDateTime = measurementDateTime;
+    }
+
+    public Sensor getSensor() {
+        return sensor;
+    }
+
+    public void setSensor(Sensor sensor) {
+        this.sensor = sensor;
+    }
+}
