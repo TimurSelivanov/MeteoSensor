45e37acb642b08b0a99613688099d3c9cbfa60b6
TimSelivan <timselivanson@gmail.com>
2023-04-05 19:07:43 +0300

    Implement repository layer

---
+++ src/main/java/com/selivanov/MeteoSensor/repository/MeasurementRepository.java
@@ -0,0 +1,9 @@
+package com.selivanov.MeteoSensor.repository;
+
+import com.selivanov.MeteoSensor.model.Measurement;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MeasurementRepository extends JpaRepository<Measurement, String> {
+}
---
+++ src/main/java/com/selivanov/MeteoSensor/repository/SensorRepository.java
@@ -0,0 +1,12 @@
+package com.selivanov.MeteoSensor.repository;
+
+import com.selivanov.MeteoSensor.model.Sensor;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface SensorRepository extends JpaRepository<Sensor, Integer> {
+    Optional<Sensor> findByName(String name);
+}
